d141e260053395ecad992159de58f3b9ceca4f47
Đào Phương Nam <ealflm@gmail.com>
2023-05-02 15:50:16 +0700

    Initial Commit

---
+++ NotionTimer/NotionTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotionTimerApp.swift
+//  NotionTimer
+//
+//  Created by Đào Phương Nam on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotionTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
